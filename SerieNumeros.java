7e160708e5854f836cac069416521fb3a939b775
Ignacio Romero <102398895+IgnaaRo@users.noreply.github.com>
2026-03-25 17:35:41 -0300

    Add files via upload

---
+++ SerieNumeros.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class SerieNumeros {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int cantidad = 500;
+        int suma = 0;
+
+        for (int i = 0; i < cantidad; i++) {
+            int numero = random.nextInt(991) + 10; 
+            suma += numero;
+        }
+
+        double promedio = (double) suma / cantidad;
+
+        System.out.println("Cantidad de números: " + cantidad);
+        System.out.println("Suma total: " + suma);
+        System.out.println("Promedio: " + promedio);
+    }
+}
